0502588063650180b4503a52ef4af3bc0ff73b94
Craig Waite <craigvl@tpg.com.au>
2015-01-20 15:15:32 +1000

    Updates

---
+++ app/scripts/app.config.js
@@ -0,0 +1,16 @@
+﻿angular.module('jwtApp').config(function ($urlRouterProvider, $stateProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+        .state('main', {
+            url: '/',
+            templateUrl:'/views/main.html'
+        })
+
+        .state('register', {
+        url: '/register',
+        templateUrl:'/views/register.html'
+    });
+})
