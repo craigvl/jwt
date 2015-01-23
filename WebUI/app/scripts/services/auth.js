50b38e7e55b8826f6e3a3a035a906e12231cc6c6
Craig <craig@192-168-1-103.tpgi.com.au>
2015-01-23 12:27:31 +1000

    Added auth service

---
+++ WebUI/app/scripts/services/auth.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name jwtApp.auth
+ * @description
+ * # auth
+ * Service in the jwtApp.
+ */
+angular.module('jwtApp')
+  .service('auth', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
