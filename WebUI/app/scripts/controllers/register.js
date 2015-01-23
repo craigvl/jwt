6c2b2f1f1b0a7e0aade2816c0ea1ff8b13a28300
Craig <craig@192-168-1-103.tpgi.com.au>
2015-01-23 12:14:27 +1000

    Update

---
+++ WebUI/app/scripts/controllers/register.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('jwtApp')
+    .controller('RegisterCtrl', function ($scope, $rootScope, $http, alert, authToken,API_URL) {
+        $scope.submit = function () {
+
+            var url = API_URL + 'register';
+            var user = {
+                email: $scope.email,
+                password: $scope.password
+            };
+
+            $http.post(url, user)
+                .success(function (res) {
+                    alert('success', 'Account Created! ', 'Welcome ' + res.user.email + '!');
+                    authToken.setToken(res.token);
+                })
+                .error(function (err) {
+                            alert('warning','Something went wrong:(:',err.message);
+
+                });
+        }
+    });
