9595e7c8acfcb566d8e32fec07cc61de09737a2c
Craig <craig@192-168-1-103.tpgi.com.au>
2015-01-29 22:15:52 +1000

    Updated to work with new sails server

--- WebUI/app/scripts/services/auth.js
+++ WebUI/app/scripts/services/auth.js
@@ -2,14 +2,14 @@
 
 angular.module('jwtApp')
     .service('auth', function auth($http, API_URL, authToken, $state) {
-      
+
         function authSuccessful(res) {
             authToken.setToken(res.token);
             $state.go('main');
         }
 
         this.login = function (email, password) {
-            return $http.post(API_URL + 'login', {
+            return $http.post('http://localhost:1337/auth/login', {
                 email: email,
                 password: password
             }).success(authSuccessful);
