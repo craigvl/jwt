fa51878b1499ba4015c4a40c353b515667ce8e15
Craig Waite <craigvl@tpg.com.au>
2015-02-04 13:04:32 +1000

    google login working.

--- WebUI/app/scripts/services/auth.js
+++ WebUI/app/scripts/services/auth.js
@@ -15,7 +15,7 @@ angular.module('jwtApp')
             }).success(authSuccessful);
         }
 
-        this.register = function (email, password, firstname, lastname,selectedlocationid) {
+        this.register = function (email, password, firstname, lastname, selectedlocationid) {
             return $http.post(API_URL + 'auth/register', {
                 email: email,
                 password: password,
@@ -25,31 +25,41 @@ angular.module('jwtApp')
             }).success(authSuccessful);
         }
         
-        this.googleAuth = function() {
+        this.googleAuth = function () {
+
+            var urlBuilder = [];
+            var clientId = '549020993769-tn974vfkrovsr1k4g65135k6m02vec6j.apps.googleusercontent.com';
+
+            urlBuilder.push('response_type=code',
+                'client_id=' + clientId,
+                'redirect_uri=' + window.location.origin,
+                'scope=profile email')
+
+            var url = "https://accounts.google.com/o/oauth2/auth?" + urlBuilder.join('&');
             var options = 'width=500,height=500,left=' + ($window.outerWidth - 500) / 2 + ',top=' + ($window.outerHeight - 500) / 2.5;
-            
+
             var deferred = $q.defer();
-            
-            var popup = $window.open('http://localhost:1337/auth/google','', options);
+
+            var popup = $window.open(url, '', options);
             $window.focus();
-            
-           	$window.addEventListener('message', function (event) {
-			 console.log(event.data);
-				var code = event.data;
-				popup.close();
 
-				$http.post(API_URL + 'auth/googlelogin', {
-					code: code,
-                    clientId: '549020993769-tn974vfkrovsr1k4g65135k6m02vec6j.apps.googleusercontent.com',
-					redirectUri: window.location.origin
-				}).success(function (jwt) {
-					authSuccessful(jwt);
-					deferred.resolve(jwt);
-				});
-			
-		});
+            $window.addEventListener('message', function (event) {
+                if (event.origin === $window.location.origin) {
+                    var code = event.data;
+                    popup.close();
+                    console.log(code);
+                    $http.post(API_URL + 'auth/google', {
+                        code: code,
+                        clientId: clientId,
+                        redirectUri: window.location.origin
+                    }).success(function (jwt) {
+                        authSuccessful(jwt);
+                        deferred.resolve(jwt);
+                    });
+                }
+            });
 
-		return deferred.promise;
+            return deferred.promise;
         }
-        
+
     });
