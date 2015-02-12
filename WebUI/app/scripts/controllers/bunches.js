7d1c432d8818aa99601572b24cc1f6a674f6079a
Craig Waite <craigvl@tpg.com.au>
2015-02-12 17:05:59 +1000

    updates.

--- WebUI/app/scripts/controllers/bunches.js
+++ WebUI/app/scripts/controllers/bunches.js
@@ -2,7 +2,7 @@
 
 angular.module('jwtApp')
   .controller('BunchesCtrl', function ($scope,$http, API_URL,alert,$state) {
-    $http.get(API_URL + 'bunch').success(function(bunches){
+    $http.get(API_URL + 'bunch/location/54cb04c4c39fcb501cb1a8ae').success(function(bunches){
      $scope.bunches = bunches;   
     }).error(function(err){
         if(err == null)
--- WebUI/app/scripts/controllers/logout.js
+++ WebUI/app/scripts/controllers/logout.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('jwtApp')
-    .controller('LogoutCtrl', function ($state, $auth) {
+    .controller('LogoutCtrl', function ($state, $auth, $http, API_URL) {
+        $http.get(API_URL + 'auth/logout/');
         $auth.logout();
         $state.go('main');
     });
