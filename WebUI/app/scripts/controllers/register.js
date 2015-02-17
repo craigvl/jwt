abe7d05e6c47cc2d58d559db9f2a86077c10d206
Craig <craig@CaritasMac-78.local>
2015-02-17 23:14:45 +1000

    Small fixes

--- WebUI/app/scripts/controllers/register.js
+++ WebUI/app/scripts/controllers/register.js
@@ -3,7 +3,7 @@
 angular.module('jwtApp')
     .controller('RegisterCtrl', function ($scope, $http, API_URL, alert, $auth, $state) {
         $scope.local = {};
-        $http.get(API_URL + 'bylocation').success(function (locations) {
+        $http.get(API_URL + 'location').success(function (locations) {
             $scope.locations = locations;
         }).error(function (err) {
             if (err == null) {
@@ -16,7 +16,13 @@ angular.module('jwtApp')
         })
 
         $scope.submit = function () {
-            $auth.signup({email:$scope.email,password: $scope.password, firstname: $scope.firstname,lastname: $scope.lastname,location: $scope.local.selected})
+            $auth.signup({
+                    email: $scope.email,
+                    password: $scope.password,
+                    firstname: $scope.firstname,
+                    lastname: $scope.lastname,
+                    location: $scope.local.selected
+                })
                 .then(function (res) {
                     alert('success', 'Account Created! ', 'Welcome ' + res.data.user.email + '! You have been sent a verification email.');
                 })
