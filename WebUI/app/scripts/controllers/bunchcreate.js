ff5d713e1d44a0f9cb53e5e009a55645861e61b2
Craig Waite <craigvl@tpg.com.au>
2015-02-24 16:12:39 +1000

    strava routes.

--- WebUI/app/scripts/controllers/bunchcreate.js
+++ WebUI/app/scripts/controllers/bunchcreate.js
@@ -3,36 +3,102 @@
 angular.module('jwtApp')
     .controller('BunchcreateCtrl', function ($scope, $http, API_URL, leafletData, alert) {
 
+        $scope.someFunction = function (item, model) {
+            console.log($scope.stravaride.selected.id);
+
+            $http.get(API_URL + 'bunch/stravaactivity?id=' + $scope.stravaride.selected.id).success(function (stravaride) {
+                $scope.cen = {
+                    lat: stravaride.startlat,
+                    lng: stravaride.startlng,
+                    zoom: 11
+                },
+                $scope.paths = {
+                    p1: {
+                        color: '#008000',
+                        weight: 4,
+                        latlngs: stravaride.routearray,
+                        layer: 'lines'
+                    }
+                }
+            });
+        };
+
         $scope.center = {};
+        $scope.paths = {};
+        $scope.cen = {};
         $scope.availableDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        $scope.hour = {};
+        $scope.stravaride = {};
         $scope.hours = ['1', '2', '3', '4', '5', '6', '7'];
         $scope.multipleSelect = {};
         $scope.multipleSelect.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
         $scope.oneoff = false;
         $scope.private = false;
 
+        $scope.layers = {
+            baselayers: {
+                osm: {
+                    name: 'OpenStreetMap',
+                    type: 'xyz',
+                    url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    layerOptions: {
+                        subdomains: ['a', 'b', 'c'],
+                        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                        continuousWorld: true
+                    }
+                }
+            },
+            overlays: {
+                lines: {
+                    name: 'Lines',
+                    type: 'group',
+                    visible: true
+                }
+            }
+        };
+
         $http.get(API_URL + 'location').success(function (locations) {
             $scope.locations = locations;
+        });
+
+        $http.get(API_URL + 'bunch/stravaactivities').success(function (stravarides) {
+            $scope.stravarides = stravarides;
+            $scope.paths = {
+                p1: {
+                    color: '#008000',
+                    weight: 4,
+                    latlngs: stravarides[1].routearray,
+                    layer: 'lines'
+                }
+            }
+
         });
 
         $http.get(API_URL + 'location/byuser').success(function (startlocation) {
             $scope.startlocation = startlocation;
             console.log(startlocation);
+
+            $scope.cen = {
+                lat: $scope.startlocation.lat,
+                lng: $scope.startlocation.lng,
+                zoom: 13
+            },
+
             $scope.center = {
+                lat: $scope.startlocation.lat,
+                lng: $scope.startlocation.lng,
+                zoom: 13
+            },
+
+            $scope.markers = {
+                mainMarker: {
                     lat: $scope.startlocation.lat,
                     lng: $scope.startlocation.lng,
-                    zoom: 13
-                },
-
-                $scope.markers = {
-                    mainMarker: {
-                        lat: $scope.startlocation.lat,
-                        lng: $scope.startlocation.lng,
-                        focus: true,
-                        message: "Where does the ride start?",
-                        draggable: true
-                    }
+                    focus: true,
+                    message: "Where does the ride meet?",
+                    draggable: true
                 }
+            }
         });
 
         $scope.$on("leafletDirectiveMap.click", function (event, args) {
@@ -42,6 +108,7 @@ angular.module('jwtApp')
         });
 
         $scope.submit = function () {
+
             console.log($scope.name);
             $http.post(API_URL + 'bunch/create', {
                 name: $scope.name
