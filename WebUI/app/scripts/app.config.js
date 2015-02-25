9b75ca6e421f46d23bc25b6707b55884eb8001b6
Craig Waite <craigvl@tpg.com.au>
2015-02-25 17:16:35 +1000

    Set location start.

--- WebUI/app/scripts/app.config.js
+++ WebUI/app/scripts/app.config.js
@@ -4,9 +4,15 @@ angular.module('jwtApp').config(function ($urlRouterProvider, $stateProvider, $h
 
     $stateProvider
 
-    .state('main', {
+        .state('main', {
         url: '/',
         templateUrl: '/views/main.html'
+    })
+
+    .state('setlocation', {
+        url: '/setlocation',
+        templateUrl: '/views/setlocation.html',
+        controller: 'SetlocationCtrl'
     })
 
     .state('register', {
@@ -26,7 +32,7 @@ angular.module('jwtApp').config(function ($urlRouterProvider, $stateProvider, $h
         templateUrl: '/views/bunches.html',
         controller: 'BunchesCtrl'
     })
-    
+
     .state('bunchcreate', {
         url: '/bunchcreate',
         templateUrl: '/views/bunchcreate.html',
@@ -37,24 +43,28 @@ angular.module('jwtApp').config(function ($urlRouterProvider, $stateProvider, $h
         url: '/logout',
         controller: 'LogoutCtrl'
     });
-    
-    $authProvider.google({clientId:'549020993769-tn974vfkrovsr1k4g65135k6m02vec6j.apps.googleusercontent.com',
-                         url: API_URL + 'auth/google'});
-    
-    $authProvider.facebook({clientId:'1403149849923216',
-                         url: API_URL + 'auth/facebook'});
-    
-     $authProvider.oauth2({
-      name: 'strava',
-      url: API_URL + 'auth/strava',
-      clientId: '4728',
-      redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
-      response_type: 'code',
-      scope:'write',
-      approval_prompt:'force',
-      authorizationEndpoint: 'https://www.strava.com/oauth/authorize'
+
+    $authProvider.google({
+        clientId: '549020993769-tn974vfkrovsr1k4g65135k6m02vec6j.apps.googleusercontent.com',
+        url: API_URL + 'auth/google'
     });
-    
+
+    $authProvider.facebook({
+        clientId: '1403149849923216',
+        url: API_URL + 'auth/facebook'
+    });
+
+    $authProvider.oauth2({
+        name: 'strava',
+        url: API_URL + 'auth/strava',
+        clientId: '4728',
+        redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+        response_type: 'code',
+        scope: 'write',
+        approval_prompt: 'force',
+        authorizationEndpoint: 'https://www.strava.com/oauth/authorize'
+    });
+
     $authProvider.loginUrl = API_URL + 'auth/login';
     $authProvider.signupUrl = API_URL + 'auth/register';
 
---
+++ WebUI/app/scripts/controllers/setlocation.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('jwtApp')
+    .controller('SetlocationCtrl', function () {
+
+    });
