69dfa041fdbe4f195a96f36ba44be768e3170145
Craig Waite <craigvl@tpg.com.au>
2015-03-16 13:34:53 +1000

    update.

--- WebUI/app/scripts/services/bunchservices.js
+++ WebUI/app/scripts/services/bunchservices.js
@@ -7,7 +7,7 @@ angular.module('jwtApp')
                 return $http.get(API_URL + 'bunch/byuserandday?id=' + id)
             },
             getBunchesByUsers: function () {
-                return $http.get(API_URL + 'bunch/byuser')
+                return $http.get(API_URL + 'bunch/byuser');
             },
             getBunchesByUserandDayOneOff: function (id) {
                 return $http.get(API_URL + 'bunch/byuseranddayoneoff?id=' + id)
